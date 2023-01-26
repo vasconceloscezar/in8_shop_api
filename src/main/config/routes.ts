300f08e4bc0b5beebf9810528b614d842a5ad531
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-26 10:56:16 -0300

    feat: add product model and parse brazilian products to our own product model

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,14 +4,15 @@ import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
-  app.use('/', (req, res) => res.status(200).send({
-    serverDate: new Date(),
-    author: 'Cezar Vasconcelos'
-  }))
+
   app.use('/api', router)
   readdirSync(join(__dirname, '../routes')).map(async file => {
     if (!file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
+  app.use('/', (req, res) => res.status(200).send({
+    serverDate: new Date(),
+    author: 'Cezar Vasconcelos'
+  }))
 }
