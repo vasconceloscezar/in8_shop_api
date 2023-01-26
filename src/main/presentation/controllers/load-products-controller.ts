77c4987bf6c180ffa35c37025cf967bfb5228f0c
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-26 11:22:01 -0300

    feat: add a function to loadProductsFromAllProviders on LoadProductsController

--- src/main/presentation/controllers/load-products-controller.ts
+++ src/main/presentation/controllers/load-products-controller.ts
@@ -1,4 +1,5 @@
 import { type ProductProvider } from '@/data/protocols'
+import { type Product } from '@/domain/models'
 import { ok, serverError } from '../helpers/http-helper'
 import { type Controller, type HttpResponse } from '../protocols'
 
@@ -7,11 +8,17 @@ export class LoadProductsController implements Controller {
     private readonly productProviders: ProductProvider[]
   ) {}
 
+  async loadProductsFromAllProviders (): Promise<Product[]> {
+    const products = await Promise.all(this.productProviders.map(async provider => provider.loadProducts()))
+    return products.flat()
+  }
+
   async handle (request: any): Promise<HttpResponse> {
     try {
-      const products = await Promise.all(this.productProviders.map(async provider => provider.loadProducts()))
+      const products = await this.loadProductsFromAllProviders()
 
       return ok({
+        totalProducts: products.length,
         products
       })
     } catch (err: any) {
