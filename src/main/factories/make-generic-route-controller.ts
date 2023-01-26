b2d1ab0a0fb6bb357220a37f69c2826e5f3887bc
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-26 10:28:23 -0300

    chore: remove generic files

--- src/main/factories/make-generic-route-controller.ts
+++
@@ -1,8 +0,0 @@
-import { GenericController } from "../presentation/controllers/generic-controller"
-import { Controller } from "../presentation/protocols"
-
-
-export const makeGenericRouteController = (): Controller => {
-  const controller = new GenericController()
-  return controller
-}
--- src/main/routes/generic-route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeGenericRouteController } from '../factories/make-generic-route-controller'
-
-export default (router: Router): void => {
-  router.get('/generic_route', adaptRoute(makeGenericRouteController()))
-}
