db3cc7ce77fec53e2a68aa02d6ffdf436839d0aa
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-01-27 17:21:54 -0300

    fix: add dotconfig for loading env variables

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,15 @@
 import 'module-alias/register'
-import env from './config/env'
+import * as dotenv from 'dotenv'
+import env from './config/env' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 setTimeout(async () => {
   const { setupApp } = await import('./config/app')
   const app = await setupApp()
   try {
-    app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
+    app.listen(env.port, () => {
+      console.log(`Server running at http://localhost:${env.port}`)
+    })
   } catch (err) {
     console.error(err)
   }
