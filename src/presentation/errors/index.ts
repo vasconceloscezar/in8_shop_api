a22d6d61cb40f57cc98e8ded260a55840517caf5
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-02-01 11:34:31 -0300

    feat: add makeSignUpValidator, new error protocols and validators

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,2 +1,5 @@
 export * from './server-error'
 export * from './unauthorized-error'
+export * from './email-in-use-error'
+export * from './invalid-param-error'
+export * from './missing-param-error'
