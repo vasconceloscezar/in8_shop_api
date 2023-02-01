e562858d593473d29b72a59eeacdd9e28da5e7f0
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-02-01 15:05:45 -0300

    feat: add auth middleware on purchase route

--- src/main/routes/purchase-route.ts
+++ src/main/routes/purchase-route.ts
@@ -1,7 +1,8 @@
 import { type Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddPurchaseController } from '../factories'
+import { auth } from '../middleware'
 
 export default (router: Router): void => {
-  router.post('/purchase', adaptRoute(makeAddPurchaseController()))
+  router.post('/purchase', auth, adaptRoute(makeAddPurchaseController()))
 }
