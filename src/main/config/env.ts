36977a5a7a35378d250a051a3f57b1b484a3e2e1
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-02-08 14:24:51 -0300

    chore: add URI for mongoDB cluster on atlas

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoURI: process.env.MONGO_URI ?? 'mongodb://host.docker.internal:27017',
+  // mongoURI: process.env.MONGO_URI ?? 'mongodb://host.docker.internal:27017',
+  mongoURI: process.env.MONGO_URI ?? 'mongodb+srv://czrvsc:XZMf92IvD4Domzd9@in8-db.d4qh0gb.mongodb.net/?retryWrites=true&w=majority',
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'tj67O==5H',
   providersURL: {
