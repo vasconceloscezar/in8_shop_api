17c1d606698bb3a905f3c8cb6cac0675a8d2e3f7
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-02-09 22:09:07 -0300

    fix: remove uri from code, use only as a ENV

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  // mongoURI: process.env.MONGO_URI ?? 'mongodb://host.docker.internal:27017',
-  mongoURI: process.env.MONGO_URI ?? 'mongodb+srv://czrvsc:XZMf92IvD4Domzd9@in8-db.d4qh0gb.mongodb.net/?retryWrites=true&w=majority',
+  mongoURI: process.env.MONGO_URI ,
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'tj67O==5H',
   providersURL: {
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,14 @@
-import 'module-alias/register'
+/* eslint-disable import/first */
 import * as dotenv from 'dotenv'
-import env from './config/env' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import { MongoHelper } from '@/infra/db/mongodb'
+import 'module-alias/register'
 dotenv.config()
+import { MongoHelper } from '@/infra/db/mongodb' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import env from './config/env'
 
-MongoHelper.connect(env.mongoURI)
+MongoHelper.connect(env.mongoURI ?? '')
   .then(async () => {
     const { setupApp } = await import('./config/app')
     const app = await setupApp()
     app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
   })
-  .catch(console.error)
+  .catch(error => { console.log(error) })
